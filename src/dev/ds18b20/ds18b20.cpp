eb7e1831e2fe5da7e9a2218e79e47a0318ce708b
Blottiere Paul <blottiere.paul@gmail.com>
2019-03-10 16:50:03 +0100

    Add ds18b20 device

---
+++ src/dev/ds18b20/ds18b20.cpp
@@ -0,0 +1,33 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include <ds18b20.h>
+
+DS18B20::DS18B20(const std::string &id, const std::string &path) {
+  std::ostringstream ss;
+  ss << path << "/" << id << "/w1_slave";
+  _path = ss.str();
+}
+
+float DS18B20::temperature() const {
+  float temperature = -1.0;
+
+  std::ifstream infile;
+  infile.open(_path);
+  if (infile) {
+    std::string line;
+    while (std::getline(infile, line)) {
+      const std::size_t idx = line.find("t=");
+      if (idx != std::string::npos) {
+        const std::string temp = line.substr(idx + 2, 5);
+        temperature = std::stof(temp.c_str()) / 1000;
+        break;
+      }
+    }
+    infile.close();
+  }
+
+  return temperature;
+}
---
+++ src/dev/ds18b20/ds18b20.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+class DS18B20 {
+ public:
+  DS18B20(const std::string &id,
+          const std::string &path = "/sys/bus/w1/devices/");
+
+  float temperature() const;
+
+ private:
+  std::string _path;
+};
